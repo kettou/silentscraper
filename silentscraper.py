695a637e3ba041066ddbfdd1b92ea3beabebd85e
sav.exe <sav28g@gmail.com>
2025-02-14 17:06:38 -0500

    Update silentscraper.py
    - focused search engine mainly on google
    - updated script to pull proxy IP address from text file as well as the user agent

--- silentscraper.py
+++ silentscraper.py
@@ -1,4 +1,4 @@
-# wpHunter v1.0.0 (beta)
+# silentscraper v1.0.0 (beta)
 
 import requests
 import random
@@ -21,216 +21,91 @@ print(' ___) | | |  __/ | | | |_ ___) | (__| | | (_| | |_) |  __/ |   ')
 print('|____/|_|_|\___|_| |_|\__|____/ \___|_|  \__,_| .__/ \___|_|   ')
 print('                                              |_|              ')
 
-
-
-
 print('Welcome to SilentScraper v1.0.0 (beta)')
 print('[+] Tool created by cryptxe')
 print('[+] contact: cryptxesec@gmail.com')
 print('[+] https://github.com/savnyc')
 print('[+] https://github.com/gl0ck18')
-print('[+] https://twitter.com/cryptxe')
 print('')
 
-# read user agents from text file
-def read_user_agents(file_path):
+print('')
+print('Enter your search query: ')
+searchQuery = input()
+
+print('')
+print('Enter the number of pages you would like to scrape: ')
+pagesToScrape = int(input())
+
+# Function to read lines from a text file
+def read_lines_from_file(file_path):
     with open(file_path, "r") as file:
-        userAgents = file.read().splitlines()  # read lines and remove new line characters
-        return userAgents
+        lines = file.read().splitlines()
+    return lines
 
-# select a random user agent from text file
-def get_random_user_agent(userAgents):
-    return random.choice(userAgents)
+# Function to select a random item from a list
+def get_random_item(items):
+    return random.choice(items)
 
-# make a request with the selected user agent
-def make_request(url, userAgents):
+# Function to make a request with a random user agent and proxy
+def make_request(url, user_agents, proxies):
     headers = {
-        'User-Agent': get_random_user_agent(userAgents)
+        'User-Agent': get_random_item(user_agents)
     }
-    response = requests.get(url, headers=headers)
+    proxy = get_random_item(proxies)
+    proxies_dict = {
+        'http': proxy,
+        'https': proxy
+    }
+    response = requests.get(url, headers=headers, proxies=proxies_dict)
     return response
 
 if __name__ == '__main__':
-    # path to user agents file
-    userAgentsFile = "userAgents.txt"
+    # Paths to user agents and proxies files
+    user_agents_file = "userAgents.txt"
+    proxies_file = "proxyList.txt"
 
-    # read user agents from text file
-    userAgents = read_user_agents(userAgentsFile)
+    # Read user agents and proxies from text files
+    user_agents = read_lines_from_file(user_agents_file)
+    proxies = read_lines_from_file(proxies_file)
 
-    # select a random user agent
-    random_user_agent = get_random_user_agent(userAgents)
+    # Select a random user agent and proxy
+    random_user_agent = get_random_item(user_agents)
+    random_proxy = get_random_item(proxies)
+
+    print('')
+    print('')
+    print(f"Random Proxy: {random_proxy}")
     print(f"Random User Agent: {random_user_agent}")
+    print('Search Query: ' + searchQuery)
+    print('Pages to Scrape: ' + str(pagesToScrape))
     print('')
 
-    # make a request with the selected user agent
-    # url = "https://www.google.com"
-    # response = make_request(url, userAgents)
+    # Example URL to scrape
+    url = "https://www.example.com"
 
-    # check the response
+    # Make a request with the selected user agent and proxy
+    # response = make_request(url, user_agents, proxies)
+
+    # Check the response
     # print(f"Response Status Code: {response.status_code}")
     # print(f"Response Content: {response.content}")
 
-print('Please select the search engine you would like to scrape:')
-print('')
-
-print('1. Google')
-print('2. Bing')
-print('3. Yahoo')
-print('4. DuckDuckGo')
-print('5. Ask')
-print('6. AOL')
-print('')
-
-searchEngineTarget = input()
-
-if searchEngineTarget == '1':
-    searchEngine = 'Google'
-    searchEngineURL = 'https://www.google.com/search?q='
-    searchEngineClass = 'g'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-
-elif searchEngineTarget == '2':
-    searchEngine = 'Bing'
-    searchEngineURL = 'https://www.bing.com/search?q='
-    searchEngineClass = 'b'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-elif searchEngineTarget == '3':
-    searchEngine = 'Yahoo'
-    searchEngineURL = 'https://search.yahoo.com/search?p='
-    searchEngineClass = 'y'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-
-elif searchEngineTarget == '4':
-    searchEngine = 'DuckDuckGo'
-    searchEngineURL = 'https://duckduckgo.com/html/?q='
-    searchEngineClass = 'd'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-
-elif searchEngineTarget == '5':
-    searchEngine = 'Ask'
-    searchEngineURL = 'https://www.ask.com/web?q='
-    searchEngineClass = 'a'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-
-elif searchEngineTarget == '6':
-    searchEngine = 'AOL'
-    searchEngineURL = 'https://search.aol.com/aol/search?q='
-    searchEngineClass = 'o'
-
-    print('')
-    print('Enter your search query: ')
-    searchQuery = input()
-
-    print('')
-    print('Enter the number of pages you would like to scrape: ')
-    pagesToScrape = int(input())
-
-    if pagesToScrape > 10:
-        print('You can only scrape up to 10 pages.')
-        sys.exit()
-    elif pagesToScrape <= 1:
-        print('You must scrape at least 1 page.')
-        sys.exit()
-    elif pagesToScrape == 1:
-        print('Scraping 1 page...')
-    elif pagesToScrape > 1:
-        print('')
-        print('Scraping ' + str(pagesToScrape) + ' pages...')
-else:
-    print('Invalid search engine selection.')
+if pagesToScrape > 10:
+    print('You can only scrape up to 10 pages.')
     sys.exit()
+elif pagesToScrape < 1:
+    print('You must scrape at least 1 page.')
+    sys.exit()
+elif pagesToScrape == 1:
+    print('Scraping 1 page...')
+elif pagesToScrape > 1:
+    print('')
+    print('Scraping ' + str(pagesToScrape) + ' pages...')
+
+    print('')
+    print('Enter your search query: ')
+    searchQuery = input()
+
+    print('')
+    print('Enter the number of pages you would like to scrape: ')
+    pagesToScrape = int(input())
